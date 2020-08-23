a8065ec69e84c5809ebc3a2c966e2d0917ec2b96
Gonçalo Neves <neves.goncaloscp9@gmail.com>
2020-08-23 16:36:29 +0100

    Created Project 2 file structure

---
+++ Project 2 - Guess The Flag/Project 2 - Guess The Flag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project 2 - Guess The Flag
+//
+//  Created by Neves on 23/08/2020.
+//  Copyright © 2020 Neves. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
